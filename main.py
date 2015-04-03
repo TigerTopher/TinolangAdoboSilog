85c100323a2916d0860af30b3e685a7ca38aaf6c
Cyan Vee <cyanvillarin@gmail.com>
2015-04-03 18:04:47 +0800

    Created Main File

---
+++ main.py
@@ -0,0 +1,14 @@
+# CS 140 MP1: The Next Iron Chef
+# Christopher Vizcarra
+# Cyan Villarin
+
+class Dish():
+	def __init__(self):
+		self.input = open("input.txt")
+		self.output = open("201310940.txt", "a")
+
+	def Start(self):
+		# blah blah blah
+
+a = Dish()
+a.Start()
